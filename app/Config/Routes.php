dfa46375c201869612b20e1a8ac71aa048a07172
Faze <faze3000@gmail.com>
2023-06-14 18:37:31 +0100

    Add missing delete route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,6 +35,7 @@ $routes->get('user/create', 'UserController::create');
 $routes->post('user/save', 'UserController::save');
 $routes->get('user/edit/(:num)', 'UserController::edit/$1');
 $routes->post('user/update/(:num)', 'UserController::update/$1');
+$routes->get('user/delete/(:num)', 'UserController::delete/$1');
 
 /*
  * --------------------------------------------------------------------
